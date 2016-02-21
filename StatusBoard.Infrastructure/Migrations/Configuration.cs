ca14c869bdef95009df8fef09f6a174fc7c07b25
Luke Bearl <luke@bearl.me>
2016-02-20 20:45:29 -0800

    adding seed methods

--- StatusBoard.Infrastructure/Migrations/Configuration.cs
+++ StatusBoard.Infrastructure/Migrations/Configuration.cs
@@ -1,3 +1,7 @@
+using StatusBoard.Core.Models;
+using StatusBoard.Core.Models.Identity;
+using StatusBoard.Infrastructure.DbContext;
+
 namespace StatusBoard.Infrastructure.Migrations
 {
     using System;
@@ -10,12 +14,14 @@ namespace StatusBoard.Infrastructure.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            
         }
 
         protected override void Seed(StatusBoard.Infrastructure.DbContext.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
-
+            context.Users.Add(new User() {EmailAddress = "lbearl@gmail.com", UserName = "lbearl"});
+            context.Servers.Add(new Server() {Hostname = "https://www.thelagirl.com", IsActive = true});
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
