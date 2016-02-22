954222f2a28436434e7f5bd0499ee3302f518e00
Luke Bearl <luke@bearl.me>
2016-02-21 18:06:52 -0800

    Switched from nuget frontend dependency management to bower. Note that I'm still using BundleConfig as I'm not happy with gulp/grunt integration right now (although I have a few ideas)

--- StatusBoard.Web/App_Start/BundleConfig.cs
+++ StatusBoard.Web/App_Start/BundleConfig.cs
@@ -8,22 +8,26 @@ namespace StatusBoard.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/bower_components/jquery/dist/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/bower_components/jquery.validation/dist/jquery.validate.js").Include(
+                        "~/Content/bower_components/Microsoft.jQuery.Unobtrusive.Validation/jquery.validate.unobtrusive.js")
+                );
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+            // ^^ Getting rid of modernizr for now, MS made a bone headed decision to package it like this
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/bower_components/bootstrap/dist/js/bootstrap.js",
+                      "~/Content/bower_components/respondJs/dest/respond.src.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bower_components/bootstrap/dist/css/bootstrap.css",
+                      "~/Content/bower_components/bootstrap/dist/css/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
