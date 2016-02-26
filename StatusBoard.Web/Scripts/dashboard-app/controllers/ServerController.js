7a7ddd83e58516d8304b90c40e58520452a9ba8b
Luke Bearl <luke@bearl.me>
2016-02-26 09:57:02 -0800

    $interval should now be properly working within angular (haven't had a chance to test it yet though)

--- StatusBoard.Web/Scripts/dashboard-app/controllers/ServerController.js
+++ StatusBoard.Web/Scripts/dashboard-app/controllers/ServerController.js
@@ -11,29 +11,28 @@ var Dashboard;
                 this.$routeParams = $routeParams;
                 this.$http = $http;
                 this.$interval = $interval;
-                $http({
-                    method: 'GET',
-                    url: '/api/ServerActions/GetServerHistoryForServer?id=' + $routeParams.serverid
-                }).then(function (response) {
-                    $scope.serverHistory = response.data.ServerHistory;
-                    $scope.chartData = response.data.TimeSeries;
-                    $scope.title = response.data.HostName;
-                    $scope.chartOptions = {
-                        chart: {
-                            type: 'sparklinePlus',
-                            height: 200,
-                            x: function (d, i) { return i; },
-                            y: function (d) { return d.PingResponseTime; },
-                            xTickFormat: function (d) { return d3.time.format('%x %X')(new Date($scope.chartData[d].TakenAt)); },
-                            duration: 250
-                        }
-                    };
-                }, function (response) {
-                    //fail
-                });
-                //forcibly injecting the servers into the scope for now
-                //$scope.serverHistory = [new ServerHistory(server, server.serverId, 'OK', '1020', 'Valid', new Date(2016, 11, 11, 12, 0)),
-                //    new ServerHistory(server, server.serverId, 'OK', '740', 'Valid', new Date(2016, 11, 11, 12, 0))];
+                $interval(function () {
+                    $http({
+                        method: 'GET',
+                        url: '/api/ServerActions/GetServerHistoryForServer?id=' + $routeParams.serverid
+                    }).then(function (response) {
+                        $scope.serverHistory = response.data.ServerHistory;
+                        $scope.chartData = response.data.TimeSeries;
+                        $scope.title = response.data.HostName;
+                        $scope.chartOptions = {
+                            chart: {
+                                type: 'sparklinePlus',
+                                height: 200,
+                                x: function (d, i) { return i; },
+                                y: function (d) { return d.PingResponseTime; },
+                                xTickFormat: function (d) { return d3.time.format('%x %X')(new Date($scope.chartData[d].TakenAt)); },
+                                duration: 250
+                            }
+                        };
+                    }, function (response) {
+                        //fail
+                    });
+                }, 5000);
             }
             ServerController.$inject = [
                 '$scope',
--- StatusBoard.Web/Scripts/dashboard-app/controllers/ServerController.ts
+++ StatusBoard.Web/Scripts/dashboard-app/controllers/ServerController.ts
@@ -19,30 +19,28 @@ module Dashboard.Controllers {
             private $routeParams: any, //the use of "any" here is a hack until I have time to build out all the ifaces needed
             private $http: any,
             private $interval: ng.IIntervalService) {
-            $http({
-                method: 'GET',
-                url: '/api/ServerActions/GetServerHistoryForServer?id=' + $routeParams.serverid
-            }).then(response => {
-                $scope.serverHistory = response.data.ServerHistory;
-                $scope.chartData = response.data.TimeSeries;
-                $scope.title = response.data.HostName;
-                $scope.chartOptions = {
-                    chart: {
-                        type: 'sparklinePlus',
-                        height: 200,
-                        x: (d, i) => i,
-                        y: (d) => d.PingResponseTime,
-                        xTickFormat: d => d3.time.format('%x %X')(new Date($scope.chartData[d].TakenAt)),
-                        duration: 250
+            $interval(() => {
+                $http({
+                    method: 'GET',
+                    url: '/api/ServerActions/GetServerHistoryForServer?id=' + $routeParams.serverid
+                }).then(response => {
+                    $scope.serverHistory = response.data.ServerHistory;
+                    $scope.chartData = response.data.TimeSeries;
+                    $scope.title = response.data.HostName;
+                    $scope.chartOptions = {
+                        chart: {
+                            type: 'sparklinePlus',
+                            height: 200,
+                            x: (d, i) => i,
+                            y: (d) => d.PingResponseTime,
+                            xTickFormat: d => d3.time.format('%x %X')(new Date($scope.chartData[d].TakenAt)),
+                            duration: 250
+                        }
                     }
-                }
-            }, response => {
-                //fail
-            });
-
-            //forcibly injecting the servers into the scope for now
-            //$scope.serverHistory = [new ServerHistory(server, server.serverId, 'OK', '1020', 'Valid', new Date(2016, 11, 11, 12, 0)),
-            //    new ServerHistory(server, server.serverId, 'OK', '740', 'Valid', new Date(2016, 11, 11, 12, 0))];
+                }, response => {
+                    //fail
+                });
+            }, 5000);
         }
     }
 
